830966e57b327318f5155abb0ad531cfafc2a032
Raxith Amin <rakshithamin178@gmail.com>
2023-11-30 19:15:44 +0530

    Initial commit

--- Synchronization/StaticSynchronization.java
+++ Synchronization/StaticSynchronization.java
@@ -1,22 +1,23 @@
 
 public class StaticSynchronization {
     public static void main(String[] args) {
+        StaticDemo s1 = new StaticDemo();
         Thread t1 = new Thread(() -> {
             for (int i = 0; i < 100; i++) {
-                StaticDemo.increment();
+                s1.increment();
             }
         });
 
         Thread t2 = new Thread(() -> {
             for (int i = 0; i < 100; i++) {
-                StaticDemo.decrement();
+                s1.decrement();
             }
         });
 
         t1.start();
         t2.start();
 
-        System.out.println("Final count value: " + StaticDemo.getCount());
+        System.out.println("Final count value: " + s1.getCount());
     }
 }
 
